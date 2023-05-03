06bbb91e5a0913f2b0c0c452c00812790bf8fe2a
Malena Guallar <malena.guallar@gmail.com>
2023-05-03 11:58:52 +0200

    mauvaise version

--- login.js
+++ login.js
@@ -1,12 +1,15 @@
 document.getElementById("form").addEventListener("submit", function(event) {
     event.preventDefault();
     let name = document.getElementById("name").value;
-    let form = document.getElementById("form");
-    let horoscope = document.getElementById("horoscope")
 
-    document.getElementById("welcome").textContent = "Bonjour " + name;
-    form.style.display = "none" ;
-    horoscope.style.display = "block"; 
+    if (name !== ""){
+        document.getElementById("welcome").textContent = "Bonjour " + name;
+        document.getElementById("welcome").classList.remove("hidden");
+        document.getElementById("horoscope").classList.remove("hidden");
+
+
+    }
+
 
 
 });
