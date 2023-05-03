2bcfb5d3b6fcf6c5d135a7d14497e59e942133b3
Jenny Chen <jenny.chenchou@gmail.com>
2023-05-03 16:48:20 +0200

    creé horloge

--- header.js
+++ header.js
@@ -1,1 +1,23 @@
 // Composant horloge
+const clock = () => {
+    let date = new Date();
+    let hour = date.getHours();
+    let min = date.getMinutes();
+    let sec = date.getSeconds();
+    let am_pm = "AM";
+
+    if(hour > 12) {
+        am_pm = "PM";
+    }
+
+    hour = hour < 10 ? "0" + hour : hour;
+    min = min < 10? "0" + min : min;
+    sec = sec < 10 ? "0" + sec : sec;
+
+    let currentTime = `${hour} : ${min} : ${sec} ${am_pm}`;
+    // console.log(currentTime);
+    document.getElementById("clock").innerHTML = currentTime;
+}
+
+setInterval(clock, 1000);
+
