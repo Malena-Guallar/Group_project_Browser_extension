0bf4ca6aa417a03466bfa63cc5f68458bb786517
Malena Guallar <malena.guallar@gmail.com>
2023-05-03 12:13:15 +0200

    main page ok

--- login.js
+++ login.js
@@ -1,15 +1,9 @@
 document.getElementById("form").addEventListener("submit", function(event) {
     event.preventDefault();
     let name = document.getElementById("name").value;
-
-    if (name !== ""){
-        document.getElementById("welcome").textContent = "Bonjour " + name;
-        document.getElementById("welcome").classList.remove("hidden");
-        document.getElementById("horoscope").classList.remove("hidden");
-
-
-    }
-
-
-
+    let form = document.getElementById("form");
+    let horoscope = document.getElementById("horoscope")
+    document.getElementById("welcome").textContent = "Bonjour " + name;
+    form.style.display = "none" ;
+    horoscope.style.display = "block";
 });
