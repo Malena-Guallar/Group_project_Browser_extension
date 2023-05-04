f5f6c6a74b35877206679be808b7eff6f9c09f62
Jenny Chen <jenny.chenchou@gmail.com>
2023-05-04 11:19:13 +0200

    creé horloge et meteo

--- header.js
+++ header.js
@@ -21,3 +21,23 @@ const clock = () => {
 
 setInterval(clock, 1000);
 
+// conposant meteo
+
+let temp = document.getElementById("temp")
+let ville = document.getElementById("ville")
+let img = document.getElementById("icon")
+
+async function meteoFunc() {
+    const meteoUrl = "http://api.weatherapi.com/v1/forecast.json?key=eeeec6c522cd48c7b7781438230405&q=Paris&days=5&aqi=no&alerts=no";
+    const dataMeteo = await fetch(meteoUrl);
+    console.log(dataMeteo)
+
+    let responseMeteo = await dataMeteo.json();
+    console.log(responseMeteo);
+
+    temp.innerHTML = `${responseMeteo.current.temp_c}°`;
+    ville.innerHTML = responseMeteo.location.name;
+    img.src = responseMeteo.current.condition.icon;
+}
+meteoFunc();
+
