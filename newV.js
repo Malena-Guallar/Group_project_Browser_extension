dbc2a40cf3985b2df9ee30b5f172291821dbecba
Malena Guallar <malena.guallar@gmail.com>
2023-05-09 11:50:08 +0200

    pb heure réglé

--- newV.js
+++ newV.js
@@ -1,16 +1,19 @@
 function getLocalStorage(){
 
     if (localStorage.length == 0){
-        console.log("vide")
-
+       
+        let bonjour = document.getElementById("horoscope")
+        bonjour.style.display="none";
         displayForm()
 
     } else { 
         let form = document.getElementById("form");
         form.style.display = "none";
+        let bonjour = document.getElementById("horoscope")
+        bonjour.style.display="block";
 
         displayHoro()
-        console.log("plein")
+
     }
 
 }
@@ -18,18 +21,18 @@ getLocalStorage()
 
 const stockageName = (name) => {
     localStorage.setItem("name", name)
-    console.log("stockage name")
+
     displayHoro()
 };
 
 const stockageSign = (sign) => {
     localStorage.setItem("sign", sign)
-    console.log('stockage sign')
+
 }
 
 function displayForm(){
 
-    console.log('fonction displayForm')
+
     document.getElementById("form").addEventListener("submit", function(event) {
     event.preventDefault();
     let name = document.getElementById("name").value;   
@@ -47,10 +50,10 @@ function displayForm(){
 
 function getApiUrl(){
 
-    console.log('fonction getApiUrl')
+
 
     const date = new Date();
-    let day = date.getDay();
+    let day = date.getUTCDate();
     let month = date.getMonth() +1;
     let year = date.getFullYear();
     if (month < 10){
@@ -60,7 +63,7 @@ function getApiUrl(){
         day = "0" + day
     }
     let currentDate = `${month}-${day}-${year}`;
-    console.log(currentDate)
+
 
     let sign = localStorage.getItem("sign")
 
@@ -68,7 +71,7 @@ function getApiUrl(){
     const apiUrl = `https://us-central1-tf-natal.cloudfunctions.net/horoscopeapi_test?token=mmEUtLATc8w_UNnHuR2&sign=${sign}&date=${currentDate}` ;
             
     async function getHoroData(){
-                console.log('fonction async getData')
+
 
                 try {
                     const response = await fetch(apiUrl);
@@ -76,32 +79,24 @@ function getApiUrl(){
                     console.log(data)
 
                         displayHoroData(data)
-                
-
                 }  
 
                 catch (error) {
                     console.log(error)
-                } 
-
+                }
             }
             getHoroData()
 
 }
 
 
-
 function displayHoro() {
 
-    console.log('fonction displayHoro')
 
     let name = localStorage.getItem("name")
     document.getElementById("welcome").textContent = "Bonjour " + name ;
     
     getApiUrl()
-
-
-
 
 }
 
@@ -109,14 +104,14 @@ function displayHoroData(data){
 
     let horoscope = data.description;
     let horoscopeDiv = document.getElementById("horoscope_container")
-    console.log(horoscope)
-    console.log('ca passe par ici')
+
 
     let para = document.createElement("p");
     para.innerHTML = horoscope;
     horoscopeDiv.appendChild(para)
 
     form.style.display = "none";
+    horoscopeDiv.style.display = "block"; 
  
 
 }
